400b3d792b97166cb72ad29e2cb9f7d6a4793bf3
txmattt <matthew.garzaa1011@gmail.com>
2024-11-20 17:19:37 -0600

    password.sh

--- password.sh
+++ password.sh
@@ -13,7 +13,7 @@ sudo sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS 7/' /etc/login.defs
 sudo sed -i 's/^PASS_WARN_AGE.*/PASS_WARN_AGE 12/' /etc/login.defs
 
 # Enforce strong passwords using PAM and libpam-cracklib
-sudo apt install libpam-cracklib -y
+sudo apt-get install libpam-cracklib -y
 sudo sed -i '/pam_cracklib.so/ s/^#//' /etc/pam.d/common-password
 sudo sed -i '/pam_cracklib.so/ s/retry=3/retry=3 minlen=12 ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1/' /etc/pam.d/common-password
 
