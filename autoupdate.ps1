4d9ae7db9cc69d20cdea097361dfcb10f00c20c4
kylie h <kyhall2125@gmail.com>
2024-11-22 20:27:48 -0600

    Update autoupdate.ps1

--- autoupdate.ps1
+++ autoupdate.ps1
@@ -1,1 +1,8 @@
+# Start Windows Update service and set it to automatically start
+Set-Service -Name wuauserv -StartupType Automatic
+Start-Service -Name wuauserv
+
+# Confirm the service status
+Get-Service -Name wuauserv
+Write-Host "Automatic Updates service has been enabled and started."
 
