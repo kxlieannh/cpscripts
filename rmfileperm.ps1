72f7ec516c7203b8a35238f99543ba005ce34c6b
kylie h <kyhall2125@gmail.com>
2024-11-25 15:15:21 -0600

    Update rmfileperm.ps1

--- rmfileperm.ps1
+++ rmfileperm.ps1
@@ -1,17 +1,17 @@
 # Define the file/folder path and user/group
 $filePath = "C:\path\to\your\file_or_folder"
-$userOrGroup = "DOMAIN\User"  # Can also be a group name like "Everyone"
+$userOrGroup = "DOMAIN\User"  # Change as needed
 
 # Get the ACL (Access Control List) of the file/folder
 $acl = Get-Acl $filePath
 
-# Create the FileSystemAccessRule object for the user or group
-$accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($userOrGroup, "FullControl", "Allow")
-
-# Remove the rule from the ACL
-$acl.RemoveAccessRule($accessRule)
+# Remove all matching rules for the specified user/group
+$acl.Access | Where-Object { $_.IdentityReference -eq $userOrGroup } | ForEach-Object {
+    $acl.RemoveAccessRule($_)
+}
 
 # Apply the modified ACL to the file/folder
 Set-Acl $filePath $acl
 
 Write-Host "Permission has been removed for $userOrGroup on $filePath"
+
